bbef699c8f77921fccf37054fe185881d46e4aca
nathanaelbles-code <nathanaelbles@gmail.com>
2026-04-09 12:18:46 +0200

    Create README.mk

---
+++ SPHpython/README.mk
@@ -0,0 +1,19 @@
+# Simulateur de fluides SPH (Surface Particle Hydrodynamics) – Python
+
+Voici une version en Python de mon simulateur de fluides en surface libre SPH.
+
+## Objectif
+Ce projet vise à simuler le comportement de fluides à l’aide de la méthode SPH. Il m’a permis de travailler sur :
+- La modélisation physique de fluides
+- La programmation orientée performance
+- Le débogage et l’optimisation des paramètres physiques (viscosité, pression…)
+
+## Contexte
+J’avais initialement commencé le projet en C, mais des résultats inattendus m’ont conduit à réimplémenter le simulateur en Python afin de mieux comprendre et tester les paramètres physiques. 
+
+## À venir
+Une version finale en C, plus performante, sera bientôt ajoutée sur ce GitHub.
+
+## Comment exécuter
+1. Installer Python 3 et la bibliothèque c2v nécessaire
+2. Lancer `main.py`
