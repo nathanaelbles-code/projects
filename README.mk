452a2dc4d0b5155b60aa587bc42e0a1ad67150d8
nathanaelbles-code <nathanaelbles@gmail.com>
2026-04-13 11:23:01 +0200

    Update README.mk

--- README.mk
+++ README.mk
@@ -1,1 +1,16 @@
-C'est le readme
+# Bonjour, je suis Nathanaël BLES
+
+Etudiant candidat à la filière ingénierie logicielle à IMT Atlantique. Passionné de programmation graphique et développement de jeux vidéo.
+
+---
+
+## Projets
+
+**Simulation de fluide SPH** · `C` `Python`
+Simulation de fluides Smoothed-particle hydrodynamics implémentée en C et en Python.
+
+**Cartoon / Anime Shaders** · `Godot` `GLSL`
+Un shader personalisé pour un style "dessin" fait sur le moteur Godot.
+
+**Platformer State Machine** · `Godot` `GDScript`
+Une structure de stateMachine pour gérer les mouvements et les contrôles d'un personnage de jeu de plateforme.
